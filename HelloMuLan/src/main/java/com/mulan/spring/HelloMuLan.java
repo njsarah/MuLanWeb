947fc40dabb2a7bb2efec4cc72aa9f2424cf5645
dongqianqian@ea-angel.com <379689211@qq.com>
2018-11-26 18:10:45 +0800

    MulanWEB commit
    v0.0

--- HelloMuLan/src/main/java/com/mulan/spring/HelloMuLan.java
+++ HelloMuLan/src/main/java/com/mulan/spring/HelloMuLan.java
@@ -9,11 +9,13 @@ import com.mulan.spring.helloworld.HelloWorldService;
 public class HelloMuLan {
 	public static void main(String[] args)
 	{
+		//读取beans文件
 		ApplicationContext context 
 		= new ClassPathXmlApplicationContext("beans.xml");
+		//从xml文件中读取对应的bean
 		HelloWorldService service =
 				(HelloWorldService) context.getBean("HelloWorldService");
-		
+		//调用该bean对应的方法
 		HelloWorld hw = service.getHelloWorld();
 		hw.sayHello();
 	}
